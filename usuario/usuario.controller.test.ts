7b84f5160f372b7478b07abdc61f91a6dd6e0d5a
Luis E Avila <54752453+luisd159@users.noreply.github.com>
2023-05-29 22:04:09 -0500

    jest

--- usuario/usuario.controller.test.ts
+++ usuario/usuario.controller.test.ts
@@ -1,17 +1,23 @@
 import { Request, Response } from "express";
 import { createUser, getUserByID } from "./usuario.controller";
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
+import User from './usuario.model'
 
 jest.mock("./usuario.model", () => ({
     __esModule: true,
     default: jest.fn().mockImplementation(() => ({
         save: jest.fn(),
-        findById: jest.fn(),
+        findOne: jest.fn(),
     })),
 }));
 
+afterEach(() => {
+    // restore the spy created with spyOn
+    jest.restoreAllMocks();
+});
+
 describe("createUser", () => {
-    it("debería crear un nuevo usuario correctamente", async () => {
+    test("debería crear un nuevo usuario correctamente", async () => {
         const req = {
             body: {
                 name: "John Doe",
@@ -19,26 +25,25 @@ describe("createUser", () => {
                 email: "john@example.com",
                 phone_number: "1234567890",
                 address: "123 Street, City",
-            }
-        } as Request;
+            },
+        }as Request;
 
         const res = {
             status: jest.fn().mockReturnThis(),
             json: jest.fn(),
         } as unknown as Response;
 
-        bcrypt.hash = jest.fn().mockResolvedValue("hashedPassword");
+        // bcrypt.hash = jest.fn().mockResolvedValue("hashedPassword");
         await createUser(req, res);
 
         expect(res.status).toHaveBeenCalledWith(201);
-        console.log(res.json)
         expect(res.json).toHaveBeenCalledWith({
             message: "Usuario creado exitosamente",
             user: expect.any(Object),
         });
     });
 
-    it("debería retornar un error al intentar crear un usuario", async () => {
+    test("debería retornar un error al intentar crear un usuario", async () => {
 
         const req = {
             body: {
@@ -53,7 +58,8 @@ describe("createUser", () => {
             json: jest.fn(),
         } as unknown as Response;
 
-        jest.spyOn(bcrypt, "hash").mockRejectedValue(new Error("Error al hashear la contraseña"));
+        // jest.spyOn(bcrypt, "hash").mockRejectedValue(new Error("Error al hashear la contraseña"));
+
         await createUser(req, res);
 
         expect(res.status).toHaveBeenCalledWith(500);
@@ -64,3 +70,73 @@ describe("createUser", () => {
     });
 
 });
+
+
+describe("getUserByID", () => {
+
+    test("Deberia Retornar el usuario encontrado", async () => {
+        const userId = "6473de83fa5e9aad82e70140";
+        const user = {
+            "_id": userId,
+            "name": "luis",
+            "password": "luis1",
+            "email": "luis@gmail.com",
+            "phone_number": "3188740119",
+            "address": "miramar",
+            "active": true
+        }
+
+        const req = {
+            params: {
+              _id: userId,
+            },
+          }as unknown as Request;
+
+          const res = {
+            json: jest.fn(),
+            status: jest.fn().mockReturnThis(),
+          } as unknown as Response;
+      
+          jest.spyOn(User, "findOne").mockResolvedValue(user);
+          await getUserByID(req, res);
+      
+        //   expect(User.findOne).toHaveBeenCalledWith({ _id: userId, active: true });
+          expect(res.status).toHaveBeenCalledWith(200);
+        //   expect(res.json).toHaveBeenCalledWith(user);
+    });
+});
+
+
+
+// describe("getUserByEmailAndPass", () => {
+//     afterEach(() => {
+//       jest.clearAllMocks();
+//     });
+  
+//     it("debería retornar el token si el usuario y la contraseña son válidos", async () => {
+//       const email = "john@example.com";
+//       const password = "password123";
+//       const user = { _id: "123", email, password: "hashedPassword", active: true };
+//       const token = "token123";
+  
+//       const req = {
+//         query: { email, password },
+//       } as unknown as Request;
+//       const res = {
+//         status: jest.fn().mockReturnThis(),
+//         json: jest.fn(),
+//       } as unknown as Response;
+  
+//       jest.spyOn(User, "findOne").mockResolvedValue(user);
+//       jest.spyOn(bcrypt, "compare").mockResolvedValue(true);
+//       jest.spyOn(getToken, "getToken").mockResolvedValue(token);
+  
+//       await getUserByEmailAndPass(req, res);
+  
+//       expect(User.findOne).toHaveBeenCalledWith({ email, active: true });
+//       expect(bcrypt.compare).toHaveBeenCalledWith(password, user.password);
+//       expect(getToken.getToken).toHaveBeenCalledWith(user._id.toHexString());
+//       expect(res.status).toHaveBeenCalledWith(200);
+//       expect(res.json).toHaveBeenCalledWith({ token });
+//     });
+// });
